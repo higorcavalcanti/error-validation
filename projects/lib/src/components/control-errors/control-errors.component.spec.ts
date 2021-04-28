4f18c66ad6f0862bd7ba3b79501a97050a9f07a6
Higor Cavalcanti <higordaniell@live.com>
2021-04-28 17:30:41 -0300

    Iniciando testes

--- projects/lib/src/components/control-errors/control-errors.component.spec.ts
+++ projects/lib/src/components/control-errors/control-errors.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ControlErrorsComponent } from './control-errors.component';
+import { ErrorValidationConfig } from '../../configs';
 
 describe('ControlErrorsComponent', () => {
   let component: ControlErrorsComponent;
@@ -8,7 +9,10 @@ describe('ControlErrorsComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ControlErrorsComponent ]
+      declarations: [ ControlErrorsComponent ],
+      providers: [
+        { provide: ErrorValidationConfig, useValue: new ErrorValidationConfig() }
+      ]
     })
     .compileComponents();
   });
--- projects/lib/src/directives/control-error-validation.directive.spec.ts
+++ projects/lib/src/directives/control-error-validation.directive.spec.ts
@@ -1,8 +1,15 @@
 import { ControlErrorValidationDirective } from './control-error-validation.directive';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-test-component',
+  template: '<input formControlName="a">'
+})
+class TestComponent {}
 
 describe('ControlErrorValidationDirective', () => {
   it('should create an instance', () => {
-    const directive = new ControlErrorValidationDirective();
+    const directive = new ControlErrorValidationDirective(null, null, null);
     expect(directive).toBeTruthy();
   });
 });
